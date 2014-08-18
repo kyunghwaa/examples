a96bb27cd32cb3768a105e3e9489ccc86765cfd2
kyunghwaa <filiidei@naver.com>
2014-08-18 11:33:11 +0900

    profilingAdvice test version #1

--- spring-example-01/src/main/java/bank/ProfilingAdvice.java
+++ spring-example-01/src/main/java/bank/ProfilingAdvice.java
@@ -7,6 +7,15 @@ public class ProfilingAdvice {
 	public Object trace(ProceedingJoinPoint joinPoint) throws Throwable {
 		String signatureString = joinPoint.getSignature().toString();
 		System.out.println(signatureString + " Start");
+		long start = System.currentTimeMillis();
+		try {
+			Object result = joinPoint.proceed();
+			return result;
+		} finally {
+			long end = System.currentTimeMillis();
+			System.out.println(signatureString + " End");
+			System.out.println(signatureString + " Execution Time: " + (end - start) + "ms");
+		}
 	}
 	
 }
